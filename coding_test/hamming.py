866dad3530eddc715b3163a49a7e473caf84fe6e
MY_dmsco <130825500+eunzzae@users.noreply.github.com>
2023-09-10 20:19:28 +0900

    hamming coding test

---
+++ coding_test/hamming.py
@@ -0,0 +1,15 @@
+# strand1과 strand2 사이의 해밍 거리 반환
+# strand1과 strand2의 길이는 동일
+def distance(strand1, strand2):
+    distance = 0
+    L = len(strand1)
+    for i in range(L):
+        if strand1[i] != strand2[i]:
+            distance += 1
+    # 차이의 갯수 반환
+    # 두 가닥의 길이가 동일하지 않을 경우, 에러를 발생시켜야 합니다.
+    raise ValueError("Strands must be of equal length.")
+
+    return distance
+
+
