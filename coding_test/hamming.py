70c5fd2e2546e2bb1c9095d879d397b286674e20
MY_dmsco <130825500+eunzzae@users.noreply.github.com>
2023-09-10 20:40:35 +0900

    hamming distance

--- coding_test/hamming.py
+++ coding_test/hamming.py
@@ -5,9 +5,8 @@ def distance(strand1, strand2):
     L = len(strand1)
     for i in range(L):
         if strand1[i] != strand2[i] :
-            distance += 1
+            # 두 가닥의 길이가 동일하지 않을 경우, 에러를 발생시켜야 합니다.
+            raise ValueError("Strands must be of equal length.")
+        distance += 1
     # 차이의 갯수 반환
     return distance
-
-# 두 가닥의 길이가 동일하지 않을 경우, 에러를 발생시켜야 합니다.
-raise ValueError("Strands must be of equal length.")
